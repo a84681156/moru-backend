b37f3ac45ff54fa595fe7bb4dfb2864932ba0e0b
Arang0109 <kmsq321@gmail.com>
2025-10-19 21:07:01 +0900

    최ìãinitial

---
+++ src/main/java/com/project/moru/MoruApplication.java
@@ -0,0 +1,13 @@
+package com.project.moru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoruApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(MoruApplication.class, args);
+  }
+  
+}
