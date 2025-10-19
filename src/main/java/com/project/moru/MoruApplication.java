6ddb3e698bf69ebc506c4e93dd61e5da016d7368
Arang0109 <kmsq321@gmail.com>
2025-10-19 21:09:07 +0900

    최초 프로젝트

--- src/main/java/com/project/moru/MoruApplication.java
+++ src/main/java/com/project/moru/MoruApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoruApplication {
+
+  // initial
   
   public static void main(String[] args) {
     SpringApplication.run(MoruApplication.class, args);
