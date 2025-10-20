8addfc4fdd0d8cca9b0a121f19a673b92ea2a5db
Arang0109 <kmsq321@gmail.com>
2025-10-20 15:51:18 +0900

    서버 초기환경 세팅

--- src/test/java/com/project/moru/MoruApplicationTests.java
+++ src/test/java/com/project/moru/MoruApplicationTests.java
@@ -3,11 +3,12 @@ package com.project.moru;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MoruApplicationTests {
   
   @Test
   void contextLoads() {
+    System.out.println("Spring Context Loaded (DB 미연결 상태)");
   }
   
 }
